8112dba250164a1f4e31aac532c5d55af47e7fd0
Abhishek Mazumder <abhishekmazumder69@gmail.com>
2024-04-14 15:41:53 +0530

    fix bugs

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,9 @@ app.use(express.urlencoded({ extended: true }));
 // cookie-parsermiddleware
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('Api is running...');
-});
+// app.get('/', (req, res) => {
+//   res.send('Api is running...');
+// });
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
